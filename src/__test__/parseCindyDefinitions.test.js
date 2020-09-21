c3ca9358f43db3928f5cbaf39208a110a89ce68c
Michael Strobel <m.strobel@posteo.de>
2020-09-21 18:39:33 +0200

    single quotes

--- src/__test__/parseCindyDefinitions.test.js
+++ src/__test__/parseCindyDefinitions.test.js
@@ -1,33 +1,33 @@
-import { parseCindyDefitions } from "../parseCindyDefintions"
+import { parseCindyDefitions } from '../parseCindyDefintions'
 
-describe("Generate Defintions map", () => {
-  test("double defintion in single file", () => {
-    const baseDir = "src/__test__/data/doubleDefintion/singleFile"
+describe('Generate Defintions map', () => {
+  test('double defintion in single file', () => {
+    const baseDir = 'src/__test__/data/doubleDefintion/singleFile'
     expect(() => parseCindyDefitions(baseDir)).toThrow(
-      "Definition already exists!"
+      'Definition already exists!'
     )
   })
 
-  test("double defintion in multiple files", () => {
-    const baseDir = "src/__test__/data/doubleDefintion/multipleFiles"
+  test('double defintion in multiple files', () => {
+    const baseDir = 'src/__test__/data/doubleDefintion/multipleFiles'
     expect(() => parseCindyDefitions(baseDir)).toThrow(
-      "Definition already exists in another file!"
+      'Definition already exists in another file!'
     )
   })
 
-  test("get definition map", () => {
-    const baseDir = "src/__test__/cindyjs/src/js"
+  test('get definition map', () => {
+    const baseDir = 'src/__test__/cindyjs/src/js'
     const defintionsMaps = parseCindyDefitions(baseDir)
 
     const expectedDefinitons = new Map(
       Object.entries({
-        CSNumber: "libcs/CSNumber.js",
-        angleUnit: "libcs/CSNumber.js",
-        angleUnitName: "libcs/CSNumber.js",
-        TWOPI: "libcs/CSNumber.js",
-        PERTWOPI: "libcs/CSNumber.js",
-        angleUnits: "libcs/CSNumber.js",
-        List: "libcs/List.js",
+        CSNumber: 'libcs/CSNumber.js',
+        angleUnit: 'libcs/CSNumber.js',
+        angleUnitName: 'libcs/CSNumber.js',
+        TWOPI: 'libcs/CSNumber.js',
+        PERTWOPI: 'libcs/CSNumber.js',
+        angleUnits: 'libcs/CSNumber.js',
+        List: 'libcs/List.js',
       })
     )
 
--- src/parseCindyDefintions.js
+++ src/parseCindyDefintions.js
@@ -1,7 +1,7 @@
-import path from "path"
-import fs from "fs"
-import fg from "fast-glob"
-import { parse } from "acorn"
+import path from 'path'
+import fs from 'fs'
+import fg from 'fast-glob'
+import { parse } from 'acorn'
 
 export function parseCindyDefitions(baseDir) {
   const fileNames = getFileNames(baseDir)
@@ -12,7 +12,7 @@ export function parseCindyDefitions(baseDir) {
 
   const cindyDefintions = definitions.reduce((acc, val) => {
     if (intersection(acc, val)) {
-      throw new Error("Definition already exists in another file!")
+      throw new Error('Definition already exists in another file!')
     }
     return new Map([...acc, ...val])
   }, new Map())
@@ -23,21 +23,21 @@ export function parseCindyDefitions(baseDir) {
 function parseOneFileAndGetDefintions(baseDir, filePath) {
   const relPath = path.relative(baseDir, filePath)
 
-  const ignoredFiles = ["Head.js", "Tail.js"]
+  const ignoredFiles = ['Head.js', 'Tail.js']
   if (ignoredFiles.includes(relPath)) {
     return new Map()
   }
 
   const content = fs.readFileSync(filePath)
 
-  const parsed = parse(content, { ecmaVersion: "latest" })
+  const parsed = parse(content, { ecmaVersion: 'latest' })
   const { body } = parsed
 
   const nameDefinitions = extractVariableDeclarations(body)
 
   const nameMap = nameDefinitions.reduce((acc, name) => {
     if (acc.has(name)) {
-      throw new Error("Definition already exists!")
+      throw new Error('Definition already exists!')
     }
     return new Map([...acc, [name, relPath]])
   }, new Map())
@@ -47,7 +47,7 @@ function parseOneFileAndGetDefintions(baseDir, filePath) {
 
 function extractVariableDeclarations(body) {
   const variableDeclarations = body.filter(
-    el => el.type === "VariableDeclaration"
+    el => el.type === 'VariableDeclaration'
   )
 
   const nameDeclarations = variableDeclarations
@@ -59,7 +59,7 @@ function extractVariableDeclarations(body) {
 }
 
 function getFileNames(dir) {
-  return fg.sync(path.join(dir, "/**/*.js"))
+  return fg.sync(path.join(dir, '/**/*.js'))
 }
 
 function intersection(a, b) {
