d149e2daacc92baedabf93e4fa1367dfd1b6c576
Michael Strobel <m.strobel@posteo.de>
2020-09-23 10:03:16 +0200

    split tests of import / export generator

--- src/__test__/generateImportExport.test.js
+++ src/__test__/generateImportExport.test.js
@@ -1,55 +1,68 @@
 import { generateImportExport } from '../generateImportExport'
 
-test('generate missing import export', async () => {
+describe('generate missing import export', () => {
   const baseDir = 'src/__test__/cindyjs/src/js'
-  const expectedImportMap = new Map([
-    [
-      'libcs/CSNumber.js',
-      new Set([
-        { import: 'General', file: 'libcs/General.js' },
-        { import: 'List', file: 'libcs/List.js' },
-      ]),
-    ],
-    [
-      'libcs/General.js',
-      new Set([
-        { import: 'CSNumber', file: 'libcs/CSNumber.js' },
-        { import: 'List', file: 'libcs/List.js' },
-      ]),
-    ],
-    [
-      'libcs/List.js',
-      new Set([
-        { import: 'CSNumber', file: 'libcs/CSNumber.js' },
-        { import: 'General', file: 'libcs/General.js' },
-      ]),
-    ],
-  ])
 
-  const expectedExportMap = new Map([
-    ['libcs/General.js', new Set(['General'])],
-    ['libcs/List.js', new Set(['List'])],
-    ['libcs/CSNumber.js', new Set(['CSNumber'])],
-  ])
+  let exportMap, importMap, notFound
+  beforeAll(async () => {
+    const importExport = await generateImportExport(baseDir)
+    exportMap = importExport.exportMap
+    importMap = importExport.importMap
+    notFound = importExport.notFound
+  })
 
-  const expectedNotFound = [
-    'instanceInvocationArguments',
-    'nada',
-    'niceprint',
-    'nada',
-    'Dict',
-    'nada',
-    'eval_helper',
-    'comp_equals',
-    'comp_almostequals',
-    'evaluateAndVal',
-  ]
+  test('import map', () => {
+    const expectedImportMap = new Map([
+      [
+        'libcs/CSNumber.js',
+        new Set([
+          { import: 'General', file: 'libcs/General.js' },
+          { import: 'List', file: 'libcs/List.js' },
+        ]),
+      ],
+      [
+        'libcs/General.js',
+        new Set([
+          { import: 'CSNumber', file: 'libcs/CSNumber.js' },
+          { import: 'List', file: 'libcs/List.js' },
+        ]),
+      ],
+      [
+        'libcs/List.js',
+        new Set([
+          { import: 'CSNumber', file: 'libcs/CSNumber.js' },
+          { import: 'General', file: 'libcs/General.js' },
+        ]),
+      ],
+    ])
 
-  const expectedMaps = {
-    importMap: expectedImportMap,
-    exportMap: expectedExportMap,
-    notFound: expectedNotFound,
-  }
+    expect(expectedImportMap).toEqual(importMap)
+  })
 
-  expect(await generateImportExport(baseDir)).toEqual(expectedMaps)
+  test('export map', () => {
+    const expectedExportMap = new Map([
+      ['libcs/General.js', new Set(['General'])],
+      ['libcs/List.js', new Set(['List'])],
+      ['libcs/CSNumber.js', new Set(['CSNumber'])],
+    ])
+
+    expect(expectedExportMap).toEqual(exportMap)
+  })
+
+  test('not found', () => {
+    const expectedNotFound = [
+      'instanceInvocationArguments',
+      'nada',
+      'niceprint',
+      'nada',
+      'Dict',
+      'nada',
+      'eval_helper',
+      'comp_equals',
+      'comp_almostequals',
+      'evaluateAndVal',
+    ]
+
+    expect(expectedNotFound).toEqual(notFound)
+  })
 })
